93625cf0c1545aeaa13123cbbb2dd22ce9f9bdc8
github username <scott.gloyna01@gmail.com>
2021-01-15 17:21:56 -0600

    commit tree c377115ba6af10483d568b2f96b2e49927d9716b

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -1,3 +1,33 @@
 class BankAccount
+    attr_reader :name
+    attr_accessor :status, :balance
+
+    @@all =[]
+    def initialize (name)
+        @name = name
+        @balance = 1000
+        @status = "open"
+        @@all << self
+    end
+
+    def deposit (amount)
+        @balance += amount
+    end
+
+    def withdwral (amount)
+        @balance -= amount
+    end
+
+    def display_balance
+        "Your balance is $#{@balance}."
+    end
+
+    def valid?
+        @status=="open"&& @balance > 0
+    end
+
+    def close_account
+        @status = "closed" 
+    end
 
 end
